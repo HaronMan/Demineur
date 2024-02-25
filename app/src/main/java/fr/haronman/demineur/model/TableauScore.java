2bf346ca171e690367b003414c18f00fe9b38031
HaronMan <h.kochbati59@gmail.com>
2024-02-25 15:45:14 +0100

    Début tableau des scores

---
+++ app/src/main/java/fr/haronman/demineur/model/TableauScore.java
@@ -0,0 +1,10 @@
+package fr.haronman.demineur.model;
+
+/**
+ * Classe permettant de manipuler le fichier de tableau des scores
+ * Si non existant, il sera automatiquement créé au lancement du jeu
+ * @author HaronMan
+ */
+public class TableauScore {
+    private static final String CHEMIN_TABLEAU = "C:\\Users\\hkoch\\Documents\\demineur\\scoreboard";
+}
